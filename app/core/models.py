b4948dcfe82d362e08d5f2206014344486d249d4
carlos.hernandez <carlos.hernandez@talent.com>
2023-08-05 14:51:15 -0500

    code formatting errors are corrected to pass flake8 review

--- app/core/models.py
+++ app/core/models.py
@@ -1,44 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+    )
 
-# Create your models here.
+
 class UserAccountManager(BaseUserManager):
     """
     Custom user account manager for the UserAccount model.
     """
     def create_user(self, email, password=None, **extra_fields):
-       
+
         if not email:
             raise ValueError("User must have an email address")
-        
+
         email = self.normalize_email(email).lower()
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password, **extra_fields):
-        
+
         user = self.create_user(email, password, **extra_fields)
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self._db )
+        user.save(using=self._db)
 
         return user
-    
-  
+
+
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-    
-    email     = models.EmailField(max_length=255, unique=True)
-    name      = models.CharField(max_length=255)
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff  = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
-    USERNAME_FIELD  = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def __str__(self):
@@ -46,7 +50,6 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         return self.name
-    
+
     def get_short_name(self):
         return self.name
-    
--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     """Tests models."""
 
@@ -13,8 +14,11 @@ class ModelTest(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
 
-        user = get_user_model().objects.create_user(email=email, password=password)
-        
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+            )
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -28,20 +32,26 @@ class ModelTest(TestCase):
             ['test4@example.COM', 'test4@example.com']
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email=email, password='sample123')
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='sample123'
+                )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises an ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
         """Test creating a superuser."""
 
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_superuser(email=email, password=password)
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+            )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
